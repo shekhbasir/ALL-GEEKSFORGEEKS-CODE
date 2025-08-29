af635bed2fb13383c3db8b45fa60458fa74f1283
shekh basir <shekhbasir9821273349@gmail.com>
2025-08-29 23:00:12 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Implement Lower Bound/implement-lower-bound.java
+++ Difficulty: Easy/Implement Lower Bound/implement-lower-bound.java
@@ -1,21 +1,20 @@
 class Solution {
     int lowerBound(int[] arr, int target) {
-        // code herearr
-        int ind=-1;
-        int n= arr.length;
-       for(int i=0;i<n;i++){
-           if(arr[i]>=target){
-               ind=i;
-               break;
-           }
-       }
-       
-       if(ind>=0){
-           return ind;
-       }else{
-           return n;
-       }
-        
-        
+        int n = arr.length;
+        int index = n;   // agar element bada hai sabse, to n return karna hai
+        int low = 0;
+        int high = n - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] >= target) {
+                index = mid;      // potential answer
+                high = mid - 1;   // left side me aur chhota dhoondho
+            } else {
+                low = mid + 1;    // right side me dhoondho
+            }
+        }
+        return index;
     }
 }
