9d2923ca919dd1784dca401c59e397919d9ca0b8
shekh basir <shekhbasir9821273349@gmail.com>
2025-09-16 06:26:05 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Armstrong Numbers/armstrong-numbers.java
+++ Difficulty: Easy/Armstrong Numbers/armstrong-numbers.java
@@ -2,20 +2,17 @@
 class Solution {
     static boolean armstrongNumber(int n) {
         // code here
-        int ham=0;
-        int pu=0;
-        int k=n;
+        int org=n;
+        int sum=0;
+        int strlen=String.valueOf(n).length();
+        
         while(n>0){
-            int x=n%10;
+            int dig=n%10;
+            sum+=Math.pow(dig,strlen);
             n=n/10;
-            pu=(x*x*x);
-            ham=ham+pu;
-            
         }
-        if(ham==k){
-            return true;
-        }else{
-            return false;
-        }
+        
+        if(sum!=org)return false;
+        return true;
     }
 }
