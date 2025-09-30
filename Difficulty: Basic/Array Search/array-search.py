b23ff13b7f0209fdee0b58996188578b2dffd07c
shekh basir <shekhbasir9821273349@gmail.com>
2025-09-30 11:45:24 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Array Search/array-search.py
@@ -0,0 +1,9 @@
+class Solution:
+    def search(self, arr, x):
+        # code here
+        if x in arr:
+            return arr.index(x);
+        else:
+            return -1
+# search(arr,x);
+        
